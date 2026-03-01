e2a638bf99b5aba7e0d31b127badbf21b59f31d5
Dileno Cimatti <dileno2010@gmail.com>
2026-02-28 22:43:53 -0300

    simplify database engine

--- app/database.py
+++ app/database.py
@@ -4,20 +4,10 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-if not DATABASE_URL:
-    raise ValueError("DATABASE_URL not set")
-
 engine = create_engine(
     DATABASE_URL,
-    pool_pre_ping=True,
-    pool_size=5,
-    max_overflow=10
+    pool_pre_ping=True
 )
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
-
+SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
