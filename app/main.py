305bb06c3551141034837a0e890fc1773dd6672d
Dileno Cimatti <dileno2010@gmail.com>
2026-02-28 22:04:52 -0300

    add db test

--- app/main.py
+++ app/main.py
@@ -9,3 +9,13 @@ app.include_router(market_router)
 @app.get("/")
 def home():
     return {"status": "StockNewsBR backend running"}
+
+
+from app.database import engine
+from sqlalchemy import text
+
+@app.get("/db-test")
+def db_test():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT 1"))
+        return {"database": "connected"}
