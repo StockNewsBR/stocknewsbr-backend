d431a9e61d91ec5016529bc3fcda28482c5524ce
Dileno Cimatti <dileno2010@gmail.com>
2026-03-01 19:11:27 -0300

    create tables automatically

--- app/main.py
+++ app/main.py
@@ -2,7 +2,13 @@ from fastapi import FastAPI
 from app.market import router as market_router
 from app.users import router as auth_router
 
+from app.database import engine
+from app.models import Base
+
 app = FastAPI(title="StockNewsBR – Inteligência de Mercado com IA")
+
+# 👇 CRIA AS TABELAS AUTOMATICAMENTE
+Base.metadata.create_all(bind=engine)
 
 app.include_router(market_router)
 app.include_router(auth_router)
